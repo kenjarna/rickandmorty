317c085464a60e07be3450a5bc29484079dab888
Kenny Jarnagin <jarnaginkenny@gmail.com>
2022-05-18 22:04:16 -0400

    Add strict mode

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./styles/App.css";
 import { Main } from "./components/main";
 import { Navigation } from "./components/navigation";
 import { Footer } from "./components/footer";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Main />
-      <Footer />
-    </div>
+    <React.StrictMode>
+      <div className="App">
+        <Navigation />
+        <Main />
+        <Footer />
+      </div>
+    </React.StrictMode>
   );
 }
 
